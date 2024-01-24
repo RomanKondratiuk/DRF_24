e683787629cb5274d1adc8b118cc419559ef9372
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2024-01-23 19:55:59 -0500

    Created model for user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class User(AbstractUser):
-    pass
+    """ This is the model for user"""
+    first_name = models.CharField(max_length=30, verbose_name='first_name')
+    last_name = models.CharField(max_length=30, verbose_name='last_name')
+    phone = models.IntegerField(verbose_name='phone_number', **NULLABLE)
+    city = models.TextField(max_length=50, verbose_name='city', **NULLABLE)
+    avatar = models.ImageField(upload_to='avatars/', verbose_name='avatar', **NULLABLE)
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
